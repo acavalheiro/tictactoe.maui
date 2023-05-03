138f1b8fd867e70701a5575ba6b8fef86d2fcbfe
André Filipe Cavalheiro <andre.cavalheiro@thevirtualforge.com>
2023-05-03 23:20:55 +0100

    Changes

---
+++ TicTacToe.Mobile/Models/TicTacToeModel.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TicTacToeModel.cs" company="The Virtual Forge">
+// Copyright (c) 2023 All Rights Reserved
+// </copyright>
+// <summary>
+// 
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TicTacToe.Mobile.Models
+{
+    using CommunityToolkit.Mvvm.ComponentModel;
+
+    /// <summary>
+    /// The tic tac toe model.
+    /// </summary>
+    public partial class TicTacToeModel : ObservableObject
+    {
+        public TicTacToeModel(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+            
+        }
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        [ObservableProperty]
+        private string _selectedText;
+
+        public bool? Player { get; set; }
+    }
+}
--- TicTacToe.Mobile/ViewModels/GameBoardViewModel.cs
+++ TicTacToe.Mobile/ViewModels/GameBoardViewModel.cs
@@ -12,37 +12,89 @@ namespace TicTacToe.Mobile.ViewModels
     using System.Collections.ObjectModel;
 
     using CommunityToolkit.Mvvm.ComponentModel;
+    using CommunityToolkit.Mvvm.Input;
+
+    using TicTacToe.Mobile.Models;
 
     public partial class GameBoardViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string _gameStatus;
+        private bool _playerTurn = false;
+
+        private int _currentMoves = 0;
         public GameBoardViewModel()
         {
-            this.Board.Clear();
-                
-                
-               this.Board.Add("1");
-               this.Board.Add("1");
-               this.Board.Add("1");
-               this.Board.Add("2");
-               this.Board.Add("2");
-               this.Board.Add("2");
-               this.Board.Add("3");
-               this.Board.Add("3");
-               this.Board.Add("3");
-
+            
+            this.SetupGameInfo();
         }
 
-        public ObservableCollection<string> Board { get; set; } = new ObservableCollection<string>
-                                                                      {
-                                                                          "1",
-                                                                          "2",
-                                                                          "3",
-                                                                          "4",
-                                                                          "5",
-                                                                          "6",
-                                                                          "7",
-                                                                          "8",
-                                                                          "9"
-                                                                      };
+        public ObservableCollection<TicTacToeModel> Board { get; set; } = new ObservableCollection<TicTacToeModel>();
+
+        private void SetupGameInfo()
+        {
+            this.Board.Clear();
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    this.Board.Add(new TicTacToeModel(x,y));
+                }
+                
+            }
+        }
+
+        [RelayCommand]
+        public void RestartGame()
+        {
+            this.GameStatus = string.Empty;
+            this.SetupGameInfo();
+        }
+
+        [RelayCommand]
+        public void SelectedItem(TicTacToeModel selectedItem)
+        {
+
+            if (selectedItem.Player.HasValue)
+                return;
+
+            selectedItem.SelectedText = this._playerTurn ? "X" : "0";
+            selectedItem.Player = this._playerTurn;
+            
+
+            this._currentMoves++;
+
+
+            if (this.CheckForWin(this._playerTurn))
+            {
+                this.GameStatus = $"Player {(this._playerTurn ? "X" : "0")} Wins";
+            }
+
+            this._playerTurn = !this._playerTurn;
+        }
+
+        private bool CheckForWin(bool player)
+        {
+            if (this._currentMoves < 5) return false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                var rows = this.Board.Where(item => item.X == i);
+
+                if (rows.All(item => item.Player == player))
+                    return true;
+
+                rows = this.Board.Where(item => item.Y == i);
+
+                if (rows.All(item => item.Player == player))
+                    return true;
+                
+            }
+
+            return (this.Board[0].Player == player && this.Board[4].Player == player && this.Board[8].Player == player) ||
+                   (this.Board[2].Player == player && this.Board[4].Player == player && this.Board[6].Player == player);
+        }
+
     }
 }
